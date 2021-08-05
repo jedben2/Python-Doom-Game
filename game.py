6e0ab5a936e433a2e5783e9cbb4af314208639d8
James <jed.watson.jedw@gmail.com>
2021-08-05 01:36:09 +0100

    Added random bullet time. Implemented killing

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 # Game
+import ursina.shaders
 from ursina.prefabs.health_bar import HealthBar
 from ursina import *
 import player, gun, monster
@@ -14,13 +15,11 @@ window.exit_button.visible = True
 
 p = player.Player()
 p_healthbar = HealthBar(value=p.health)
-p_healthbar.position = Vec2(-.15 / 2, .15/2)
+p_healthbar.position = Vec2(-.15 / 2, .15 / 2)
 p_healthbar.scale_x = .15
 
-
 g = gun.Gun(p)
-floor = Entity(model='quad', position=Vec2(0, -1), scale_x=50, collider='box')
-m = monster.Monster(position=Vec2(-10, 0), type="small")
+floor = Entity(model='quad', position=Vec2(0, -1), scale_x=50, collider='box', texture='grass')
 
 airtime_counter = Text(text=str(p.airtime))
 airtime_counter.scale = 3
@@ -29,15 +28,33 @@ camera.position = (p.x, p.y, -10)
 camera.add_script(SmoothFollow(target=p, offset=[0, 0, -40], speed=20))
 
 bullets = []
+monsters = []
+
+monsters.append(monster.Monster(position=Vec2(-10, 0), type="small"))
+monsters.append(monster.Monster(position=Vec2(10, 0), type="small"))
+
+shot_time = 0
+
 
 def update():
+    global shot_time
     p.move(floor)
-    m.move(p, floor)
+
+    for monster in monsters:
+        monster.move(p, floor)
+        if monster.enabled == False: monsters.remove(monster)
+
     g.attach(p)
     g.look_at_mouse()
-    if held_keys['left mouse']: bullets.append(gun.Bullet(g))
+
+    if held_keys['left mouse'] and shot_time > random.randint(1, 4):
+        bullets.append(gun.Bullet(g))
+        shot_time = 0
+    shot_time += 1
     for bullet in bullets:
+        bullet.travel(monsters, floor)
         if bullet.enabled == False: bullets.remove(bullet)
+
     airtime_counter.text = str(int(np.floor(p.airtime)))
     p_healthbar.value = p.health
 
