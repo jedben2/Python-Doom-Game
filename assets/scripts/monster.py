26e1759f0ef595f3f4df52250e22112b2fd2fe57
James <jed.watson.jedw@gmail.com>
2021-08-06 17:06:01 +0100

    monster shakes when hit

--- assets/scripts/monster.py
+++ assets/scripts/monster.py
@@ -5,8 +5,10 @@ from ursina import *
 class Monster(Entity):
     dx = dy = 0
     dt = 1 / 120
-    attack_delay = 10
+    attack_delay = 6
     frame = 0
+    direction = "right"
+    dmg = 1
 
     def __init__(self, position, type, speed):
         super().__init__()
@@ -20,29 +22,41 @@ class Monster(Entity):
             self.health = 30
             if self.speed == "run":
                 self.scale = 1.2
+                self.scale_x = 1
+                self.dmg = 2
+
         elif self.TYPE == "medium":
             self.scale = 1.5
             self.health = 60
+            self.dmg = 4
             if self.speed == "run":
                 self.scale = 1.8
                 self.scale_x = 1.6
+                self.dmg = 8
+
         elif self.TYPE == "large":
             self.scale = 2
             self.health = 120
+            self.dmg = 10
 
-    def move(self, p, floor):
+    def move(self, p, floor, camera):
+        attacking = False
+
         if self.TYPE == "small": self.y += .05
         elif self.TYPE == "medium": self.y  += -.19
-        self.dx = 0
+
         if self.intersects(p).hit:
-            self.attack(p)
+            self.attack(p, camera)
+
         else:
             if self.x > p.x:
+                self.direction = "left"
                 if self.speed == "walk":
                     self.dx = -.04
                 else:
                     self.dx = -.13
             else:
+                self.direction = "right"
                 if self.speed == "walk":
                     self.dx = .04
                 else:
@@ -55,18 +69,13 @@ class Monster(Entity):
         else:
             self.dy -= 9.81 * self.dt
 
-        if self.attack_delay > 0: self.attack_delay -= 1
+        if self.attack_delay < 10: self.attack_delay += 1
 
         if self.health <= 0:
             print("MONSTER DEAD")
             self.disable()
 
         if self.dx != 0:
-            if self.dx > 0:
-                self.direction = "right"
-            else:
-                self.direction = "left"
-
             if self.frame > 22:
                 self.frame = 0
             if self.frame % 2 == 0:
@@ -84,7 +93,13 @@ class Monster(Entity):
             else:
                 self.y -= -.42
 
-    def attack(self, p):
-        if self.attack_delay == 0:
-            self.attack_delay = 10
-            p.health -= 2
+    def attack(self, p, camera):
+        if self.attack_delay == 10:
+            self.attack_delay = 0
+            p.health -= self.dmg
+
+            if p.direction == "right":
+                p.dx = -0.3
+            else:
+                p.dx = 0.3
+            camera.shake(0.1)
